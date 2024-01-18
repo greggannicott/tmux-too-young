4d2891d150b3cb94cce1b823d89dd85cf052f5a9
Greg Gannicott <greg.gannicott@precisely.com>
2024-01-18 12:26:16 +0000

    refactor: Extract fzf code out into own file

---
+++ src/fzf.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func getSelectionFromFzf(initialSearchTerm string) project {
+	var input string
+	for _, choice := range projects {
+		input += choice.getFriendlyName() + "\n"
+	}
+	cmd := exec.Command("fzf-tmux", "-p", "--cycle", "--reverse", "--border", "--info=inline-right", "--header=Select a Project to open in tmux:", "--query="+initialSearchTerm, "-1")
+	cmd.Stdin = strings.NewReader(input)
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		// If it exited with code 130, then the user pressed Ctrl-C and we don't want to show an error
+		if err.Error() == "exit status 130" {
+			os.Exit(0)
+		} else {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+	}
+	selectedProjectName := strings.TrimSpace(stdout.String())
+	return findProjectDirectoryByFriendlyName(selectedProjectName)
+}
+
+func findProjectDirectoryByFriendlyName(name string) project {
+	var matchingProjectDirectory project
+	for _, p := range projects {
+		if p.getFriendlyName() == name {
+			matchingProjectDirectory = p
+			break
+		}
+	}
+	return matchingProjectDirectory
+}
--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
@@ -126,30 +125,6 @@ func projectHasTmuxpFile(basePath string) bool {
 	return err == nil
 }
 
-func getSelectionFromFzf(initialSearchTerm string) project {
-	var input string
-	for _, choice := range projects {
-		input += choice.getFriendlyName() + "\n"
-	}
-	cmd := exec.Command("fzf-tmux", "-p", "--cycle", "--reverse", "--border", "--info=inline-right", "--header=Select a Project to open in tmux:", "--query="+initialSearchTerm, "-1")
-	cmd.Stdin = strings.NewReader(input)
-	var stdout bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = os.Stderr
-
-	err := cmd.Run()
-	if err != nil {
-		// If it exited with code 130, then the user pressed Ctrl-C and we don't want to show an error
-		if err.Error() == "exit status 130" {
-			os.Exit(0)
-		} else {
-			fmt.Println("Error:", err)
-			os.Exit(1)
-		}
-	}
-	selectedProjectName := strings.TrimSpace(stdout.String())
-	return findProjectDirectoryByFriendlyName(selectedProjectName)
-}
 func openProject(selectedProject project) {
 	if selectedProject.supportsTmuxp == true {
 		openProjectUsingTmuxp(selectedProject)
@@ -218,17 +193,6 @@ func attachToProjectFromOutsideOfTmux(p project) {
 	if err != nil {
 		fmt.Println("Error attaching to exiting session:", err)
 	}
-}
-
-func findProjectDirectoryByFriendlyName(name string) project {
-	var matchingProjectDirectory project
-	for _, p := range projects {
-		if p.getFriendlyName() == name {
-			matchingProjectDirectory = p
-			break
-		}
-	}
-	return matchingProjectDirectory
 }
 
 func sessionIsUnderway(p project) bool {
