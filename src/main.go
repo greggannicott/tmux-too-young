6ae7727f279fb4e04ce12ce5852dcb849c8938f0
Greg Gannicott <greg.gannicott@precisely.com>
2024-01-24 17:02:49 +0000

    feat: Remove all logging

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log/slog"
 	"os"
 	"strings"
 )
@@ -12,12 +11,10 @@ type worktreeDetails struct {
 }
 
 var projects []project
-var logger *slog.Logger
 
 // To run in terminal: go run tmux-too-young
 // Config file: ~/.tmux-too-young.yaml
 func main() {
-	setupLogging()
 	initialSearchTerm := getInitialSearchTerm()
 	config := getConfig()
 	scanProjectDirectories(config)
@@ -26,11 +23,5 @@ func main() {
 }
 
 func getInitialSearchTerm() string {
-	st := strings.Join(os.Args[1:], " ")
-	if st != "" {
-		logger.Info("Initial search term: " + st)
-	} else {
-		logger.Info("No initial search term provided.")
-	}
-	return st
+	return strings.Join(os.Args[1:], " ")
 }
