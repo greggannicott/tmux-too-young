47d652c50b3719e26943e0d3e1f79612d65c4958
Greg Gannicott <greg@gannicott.co.uk>
2024-05-17 13:18:44 +0100

    feat: Remove 'q' as quit binding as user will want to type that

--- tui/tui.go
+++ tui/tui.go
@@ -36,8 +36,8 @@ func (k keyMap) FullHelp() [][]key.Binding {
 
 var DefaultKeyMap = keyMap{
 	Quit: key.NewBinding(
-		key.WithKeys("q", "ctrl+c"),
-		key.WithHelp("q", "quit"),
+		key.WithKeys("ctrl+c"),
+		key.WithHelp("ctrl+c", "quit"),
 	),
 	Up: key.NewBinding(
 		key.WithKeys("ctrl+k", "up"),
